3528a7dd3618b5f3548c6cce6dc87452616a2423
DivitTas <divit.nt@gmail.com>
2026-04-13 19:12:25 +0530

    implement basic blockchain class

---
+++ blockchain.py
@@ -0,0 +1,30 @@
+import json
+import hashlib
+from datetime import datetime
+
+class Blockchain:
+    def __init__(self):
+        self.chain= []
+        self.pending_transactions = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis = self.create_block(previous_hash=0, proof=1)
+        return genesis 
+
+    def create_block(self, previous_hash, proof):
+        block = {
+            "index" : len(self.chain)+1,
+            "timestamp" : str(datetime.now()),
+            "transactions" : self.pending_transactions,
+            "previous_hash" : previous_hash,
+            "proof": proof
+        }
+        self.pending_transactions = []
+        block["hash"] = self.hash_block(block)
+        self.chain.append(block)
+        return block
+
+    def hash_block(self, block):
+        encoded = json.dumps(block,sort_keys=True).encode()
+        return hashlib.sha256(encoded).hexdigest()
